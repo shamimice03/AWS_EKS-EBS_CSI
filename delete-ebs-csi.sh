6d4f96ac2ec75241e7456197231839b1f2cd666c
Mohammed Shamim <shamimice03@gmail.com>
2022-09-18 21:13:43 +0900

    Update delete-ebs-csi.sh

--- delete-ebs-csi.sh
+++ delete-ebs-csi.sh
@@ -8,11 +8,13 @@ export POLICY_NAME=AwsEBSCSIDriverPolicy
 export POLICY_ARN=$(aws iam list-policies --query 'Policies[?PolicyName==`AwsEBSCSIDriverPolicy`].Arn' --output text)
 export ROLE_ARN=$(aws iam list-roles --query 'Roles[?RoleName==`AmazonEKS_EBS_CSI_DriverRole`].Arn' --output text)
 
+echo "Deleting IAM Role and Policies"
 # Delete IAM Role and Policies
 aws iam detach-role-policy --role-name=${ROLE_NAME}  --policy-arn=${POLICY_ARN}
 aws iam delete-role --role-name=${ROLE_NAME}
 aws iam delete-policy --policy-arn=${POLICY_ARN}
 
+echo "Deleting Service Account"
 # Delete the service account
 kubectl delete sa -n kube-system ${SA_NAME}
 
